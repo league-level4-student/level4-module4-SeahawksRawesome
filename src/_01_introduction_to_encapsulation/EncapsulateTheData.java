b17751661dfc6e10b14d24877a3f68b4073c42e0
League Student <league@WTS14-3.attlocal.net>
2019-10-27 17:21:58 -0700

    Halfway-ish thru mod 4
    yaya

--- src/_01_introduction_to_encapsulation/EncapsulateTheData.java
+++ src/_01_introduction_to_encapsulation/EncapsulateTheData.java
@@ -1,5 +1,11 @@
 package _01_introduction_to_encapsulation;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.awt.ItemSelectable;
+
+import org.junit.jupiter.api.Test;
+
 /*
  * Encapsulation is a way of protecting the data in a class from being
  * unintentionally altered from another class.
@@ -28,24 +34,76 @@ public class EncapsulateTheData {
 	private String nomenclature = " "; //must not be set to a blank string. Blank Strings get set to a space
 	private Object memberObj;  //must not be a String.  If it is a String, set it equal to a new Object();
 	
+	
 	public static void main(String[] args) {
+		//test();
+	}
+	@Test
+	void test() {
+		EncapsulateTheData tester = new EncapsulateTheData();
+		tester.setItemsReceived(-3);
+		assertEquals(0, tester.getItemsReceived());
 		
+		tester.setdegreesTurned(372);
+		assertEquals(360, tester.getdegreesTurned());
+		
+		tester.setnomenclature("");
+		assertEquals(" ", tester.getnomenclature());
+		
+		Object obj = new Object();
+		tester.setmemebrObj("jjj");
+		assertEquals(obj, tester.getmemebrObj());
 	}
 	
 	public void setItemsReceived(int itemsReceived) {
+		if(itemsReceived < 0) {
+			itemsReceived = 0;
+		}
 		this.itemsReceived = itemsReceived;
 		
 	}
 	public void setdegreesTurned(float degreesTurned) {
+		if(degreesTurned < 0) {
+			degreesTurned = 0;
+		}
+		if(degreesTurned > 360) {
+			degreesTurned = 360;
+		}
 		this.degreesTurned = degreesTurned;
 		
 	}
 	public void setnomenclature(String nomenclature) {
+		if(nomenclature.equals("")) {
+			nomenclature = " ";
+		}
 		this.nomenclature = nomenclature;
 		
 	}
 	public void setmemebrObj(Object memberObj) {
-		this.memberObj = memberObj;
+		if(memberObj.getClass().isPrimitive()) {
+			this.memberObj = memberObj;
+		}
+		else {
+			this.memberObj = new Object();
+		}
+		
 		
 	}
+	
+	
+	
+	public int getItemsReceived() {
+		return itemsReceived;
+		
+	}
+	public float getdegreesTurned() {
+		return degreesTurned;
+	}
+	public String getnomenclature() {
+		return nomenclature;
+	}
+	public Object getmemebrObj() {
+		return memberObj;
+	}
+	
 }
