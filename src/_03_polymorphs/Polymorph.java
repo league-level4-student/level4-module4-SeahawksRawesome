db500db2f43cdb52c2dcccb6f02fad3312658563
League Student <league@iMac.attlocal.net>
2019-11-03 17:22:27 -0800

    On the 4rth part of the third package
    Yay!

--- src/_03_polymorphs/Polymorph.java
+++ src/_03_polymorphs/Polymorph.java
@@ -3,8 +3,11 @@ package _03_polymorphs;
 import java.awt.Graphics;
 
 public abstract class Polymorph {
-   private int x;
-   private int y;
+   protected int x;
+   protected int y;
+   //both 50
+   protected int width = 50;
+   protected int height = 50;
     
     Polymorph(int x, int y){
    	 this.x = x;
@@ -16,4 +19,30 @@ public abstract class Polymorph {
     }
     
     public abstract void draw(Graphics g);
+    
+    public void setX(int x) {
+    	this.x = x;
+    }
+    public void setY(int y) {
+    	this.y = y;
+    }
+    public void setWidth(int width) {
+    	this.width = width;
+    }
+    public void setHeight(int height) {
+    	this.height = height;
+    }
+    
+    public int getX() {
+    	return x;
+    }
+    public int getY() {
+    	return y;
+    }
+    public int getWidth() {
+    	return width;
+    }
+    public int getHeight() {
+    	return height;
+    }
 }
